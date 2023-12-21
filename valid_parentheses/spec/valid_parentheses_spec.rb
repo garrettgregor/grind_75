1c7aa5680fe8e5f33f103f562629c0e9f7c7b6bb
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-21 16:32:37 -0500

    test: is_valid? with edge cases

---
+++ valid_parentheses/spec/valid_parentheses_spec.rb
@@ -0,0 +1,19 @@
+require "./lib/valid_parentheses.rb"
+
+RSpec.describe ValidParentheses do
+  let!(:instance) { ValidParentheses.new }
+
+  describe "instance methods" do
+    context "is_valid?" do
+      it "returns a boolean value based on whether the string is valid" do
+        expect(instance.is_valid?("()")).to be(true)
+        expect(instance.is_valid?("()[]{}")).to be(true)
+        expect(instance.is_valid?("(]")).to be(false)
+        expect(instance.is_valid?("[")).to be(false)
+        expect(instance.is_valid?("((")).to be(false)
+        expect(instance.is_valid?("(())")).to be(true)
+        expect(instance.is_valid?("{[]}")).to be(true)
+      end
+    end
+  end
+end
