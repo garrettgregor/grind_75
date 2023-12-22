cd8c93d23faa39b98e805d8512cf295eb26db39e
Garrett Gregor <garrett.gregor@gmail.com>
2023-12-22 11:11:20 -0500

    test: #max_profit

---
+++ wk_01/stock_prices/spec/stock_spec.rb
@@ -0,0 +1,23 @@
+require "./lib/stock.rb"
+
+RSpec.describe Stock do
+  let!(:instance) { Stock.new }
+
+  describe "instacnce methods" do
+    context "#max_profit" do
+      it "returns an integer for the maximum profit that can be made from a series of stock prices" do
+        prices = [7,1,5,3,6,4]
+        result = 5
+
+        expect(instance.max_profit(prices)).to eq(result)
+      end
+
+      xit "returns a value of zero if a profit can't be made" do
+        prices = [7,6,4,3,1]
+        result = 0
+
+        expect(instance.max_profit(prices)).to eq(result)
+      end
+    end
+  end
+end
