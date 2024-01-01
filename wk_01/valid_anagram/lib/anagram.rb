05f910830e3202e94e367705e50c6dc98ccb62d7
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-01 13:59:07 -0700

    feat: #is_anagram?

--- wk_01/valid_anagram/lib/anagram.rb
+++ wk_01/valid_anagram/lib/anagram.rb
@@ -1,7 +1,12 @@
 class Anagram
   def is_anagram?(s, t)
-    t.chars.permutation(t.length) do |permutation|
-      return true if permutation.join == s
-    end
+    # lowers time complexity enough; approx 283ms
+    # t.chars.sort == s.chars.sort
+
+    # # the difference between bytes and each byte is approx 90ms on LeetCode
+    # t.bytes.tally == s.bytes.tally # approx 160ms
+
+    # lowers time complexity most; approx 70ms
+    t.each_byte.tally == s.each_byte.tally
   end
 end
