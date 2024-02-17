a978dcf2d80602727d4c8ba796d1bbc48e2b540a
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-17 12:48:07 -0700

    remove comments

--- wk_01/valid_parentheses/lib/valid_parentheses.rb
+++ wk_01/valid_parentheses/lib/valid_parentheses.rb
@@ -16,37 +16,5 @@ class ValidParentheses
       end
     end
     return stack.empty?
-
-    # dictionary = {
-    #   "(" => ")",
-    #   "{" => "}",
-    #   "[" => "]"
-    # }
-    #   balanced = /
-    #   ^            # the regular expression is going to start with the instruction that is after that
-    #     (          # start of the group 1
-    #       \(\)     # means that we are expecting ()
-    #     )*         # end of the group 1, 0 or more times
-    #   $            # the regular expression is going to finish with the instruction that is before that
-    #  /x            # this is used to be able to define a regex with spaces and line breaks
-
-
-    # balanced = /^(\(\))*$/
-
-    # s.match?(balanced)
-
-    # if s.length < 2
-    #   return false
-    # else
-    #   s.chars.each_with_index do |char, index|
-    #     # check if the character exists in the dictionary
-    #     if dictionary.has_key?(char)
-    #       # check the next character matches the successive character
-    #       return s[index + 1] == dictionary[char]
-    #     else
-    #       return false
-    #     end
-    #   end
-    # end
   end
 end
