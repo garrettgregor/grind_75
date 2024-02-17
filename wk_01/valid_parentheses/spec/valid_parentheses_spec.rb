987ebdb5efa2c3a513ced32c5123d20f56c225df
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-17 12:47:55 -0700

    test: add test cases

--- wk_01/valid_parentheses/spec/valid_parentheses_spec.rb
+++ wk_01/valid_parentheses/spec/valid_parentheses_spec.rb
@@ -8,11 +8,14 @@ RSpec.describe ValidParentheses do
       it "returns a boolean value based on whether the string is valid" do
         expect(instance.is_valid?("()")).to be(true)
         expect(instance.is_valid?("()[]{}")).to be(true)
+        expect(instance.is_valid?("([{}[]])")).to be(true)
         expect(instance.is_valid?("(]")).to be(false)
+        expect(instance.is_valid?("()]")).to be(false)
         expect(instance.is_valid?("[")).to be(false)
         expect(instance.is_valid?("((")).to be(false)
         expect(instance.is_valid?("(())")).to be(true)
         expect(instance.is_valid?("{[]}")).to be(true)
+        expect(instance.is_valid?("{1[23]4}")).to be(true)
       end
     end
   end
